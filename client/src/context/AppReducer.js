44688c6c7982b468ed4e34cff52b14b250a1711a
Ayodeji_PC <dejiodetayo@gmail.com>
2021-01-18 14:25:37 +0100

    changes made

---
+++ client/src/context/AppReducer.js
@@ -0,0 +1,32 @@
+export default (state, action) => {
+  switch (action.type) {
+    case "GET_TRANSACTIONS":
+      return {
+        ...state,
+        transactions: action.payload,
+        loading: true,
+      };
+
+    case "DELETE_TRANSACTION":
+      return {
+        ...state,
+        transactions: state.transactions.filter(
+          (transaction) => transaction._id !== action.payload
+        ),
+      };
+
+    case "ADD_TRANSACTION":
+      return {
+        ...state,
+        transactions: [...state.transactions, action.payload],
+      };
+
+    case "TRANSACTION ERROR":
+      return {
+        ...state,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
